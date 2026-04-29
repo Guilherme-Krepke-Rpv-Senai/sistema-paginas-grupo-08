b86cd0cd4afd5f13a22b4a01fe96efa6e4150fa8
Weislla Fernanda da Cruz Tomazeto <fernandatomazeto@gmail.com>
2026-04-28 21:28:35 -0300

    chore: adiciona meu nome no Footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,7 +9,7 @@
  */
 const equipe: { nome: string; github: string }[] = [
   // ====== INÍCIO DA ÁREA DE EDIÇÃO COLABORATIVA ======
-
+{ nome: "Weislla Fernanda da Cruz Tomazeto", github: "nandazzq" },
   // ====== FIM DA ÁREA DE EDIÇÃO COLABORATIVA ======
 ];
 
